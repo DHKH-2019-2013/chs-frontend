bf521e28c65d224c768a77d0b68fc842344640bd
nipponwfm <nipponwfm@gmail.com>
2023-05-20 11:11:52 +0700

    [feat] add move point for en passant

--- src/app/entities/chessman/pawn/pawn.ts
+++ src/app/entities/chessman/pawn/pawn.ts
@@ -1,3 +1,4 @@
+import { BoardHistories } from "../../../component/board/board.component.i";
 import { getCode } from "../../../utils/utils";
 import { Board } from "../../board/board";
 import { Position } from "../../board/board.i";
@@ -8,7 +9,7 @@ export class Pawn extends Chessman {
     super(imageUrl, side, code);
   }
 
-  move(boardData: Record<string, Position>, currentPosition: string): Array<string> {
+  move(boardData: Record<string, Position>, currentPosition: string, histories?: BoardHistories): Array<string> {
     const moveContainer: Array<string> = [];
     const code = currentPosition.split("");
     const indexRow = Number(code[1]);
@@ -42,6 +43,27 @@ export class Pawn extends Chessman {
     _code = getCode(indexColumn - 1, indexRow + 1 * expression);
     _check = Board.checkMove(boardData, _code, this.getSide());
     if (_check.isMoveAble && _check.isEnemy) moveContainer.push(_code);
+
+    // handle en passent
+    const lastPostion = histories[histories.length - 1]?.currentPos;
+    const lastMove = histories[histories.length - 1]?.nextPos;
+    if (
+      lastPostion &&
+      lastMove &&
+      lastMove[0] === lastPostion[0] &&
+      Math.abs(+lastMove[1] - +lastPostion[1]) === 2
+    ) {
+      const currentColumn = lastMove[0];
+      if (boardData[currentPosition].object.get().code === "P" && currentPosition[1] === "5") {
+        if (Math.abs(currentColumn.charCodeAt(0) - currentPosition.charCodeAt(0)) === 1) {
+          moveContainer.push(lastMove[0] + String(+lastMove[1] + 1));
+        }
+      } else if (boardData[currentPosition].object.get().code === "p" && currentPosition[1] === "4") {
+        if (Math.abs(currentColumn.charCodeAt(0) - currentPosition.charCodeAt(0)) === 1) {
+          moveContainer.push(lastMove[0] + String(+lastMove[1] - 1));
+        }
+      }
+    }
 
     return moveContainer;
   }
